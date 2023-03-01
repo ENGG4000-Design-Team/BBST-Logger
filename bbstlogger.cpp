e390086062b0574641d4943e62e0104a51e961c3
EthanG78 <ethangarnier78@gmail.com>
2023-03-01 11:27:02 -0400

    implemented thread safe logfile writing function so each thread can write data to the log file.

--- bbstlogger.cpp
+++ bbstlogger.cpp
@@ -1,42 +1,80 @@
 /**
  * bbstlogger.cpp
  * Author: Ethan Garnier
- * Data logging application for the 
+ * Data logging application for the
  * Balloon Borne Solar Telescope (BBST)
-*/
+ */
 #include <iostream>
+#include <fstream>
 #include <thread>
 #include <vector>
 #include <string>
+#include <mutex>
+#include <chrono>
+#include <ctime>
 
 #include <opencv2/opencv.hpp>
-//#include "cmps14.hpp"
-//#include <wiringPi.h>
-//#include <wiringSerial.h>
+// #include "cmps14.hpp"
+// #include <wiringPi.h>
+// #include <wiringSerial.h>
 
-void IMUThread(std::string logFile)
+const std::string LOGFILE = "log.csv";
+std::mutex m_logfile;
+
+// Write a vector of data to logfile as a comma separated line
+// with timestamp. vector holds strings, this might suck so review this.
+void logfileWrite(const std::vector<std::string> &data)
 {
-    std::cout << "IMU THREAD" << std::endl;
+    // Lock the logfile mutex to ensure only one thread is
+    // writing to the logfile at a time. lock is automatically
+    // released when we leave the scope of function
+    const std::lock_guard<std::mutex> lock(m_logfile);
+
+    // Open logfile for append operation
+    std::ofstream logfile(LOGFILE, std::ios::app);
+
+    // Get current timestamp from system clock
+    std::time_t timestamp = std::chrono::system_clock::to_time_t(
+        std::chrono::system_clock::now());
+
+    std::string timestampStr = std::ctime(&timestamp);
+
+    // Start writing to logfile
+
+    // Goofy substring to remove newline at end of logfile
+    logfile << timestampStr.substr(0, timestampStr.length() - 1) << ", ";
+
+    for (const auto &x : data)
+        logfile << x << ",";
+
+    logfile << "\n";
+
+    logfile.close();
 }
 
-void photodiodeThread(std::string logFile)
+void IMUThread()
 {
-    std::cout << "PHOTODIODE THREAD" << std::endl;
+    std::vector<std::string> data{"heading: 213213", "pitch: 21312", "roll: 231"};
+    logfileWrite(data);
+}
+
+void photodiodeThread()
+{
+    std::vector<std::string> data{"d7", "d4", "d1"};
+    logfileWrite(data);
 }
 
 void imgProcThread()
 {
-    std::cout << "IMAGE PROCESSING THREAD" << std::endl;
+    std::vector<std::string> data{"to the right", "below", "up"};
+    logfileWrite(data);
 }
 
 int main()
 {
-    // File that data will be logged to
-    std::string logFile = "log.csv";
-
     // Launch threads
-    std::thread t_imu(IMUThread, logFile);
-    std::thread t_photodiode(photodiodeThread, logFile);
+    std::thread t_imu(IMUThread);
+    std::thread t_photodiode(photodiodeThread);
     std::thread t_imgProc(imgProcThread);
 
     // Join threads
