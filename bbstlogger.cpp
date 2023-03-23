0bf4745750a8faeff4b032907be3437832a14e78
EthanG78 <ethangarnier78@gmail.com>
2023-03-23 15:22:23 -0300

    porting over photodiode code

--- bbstlogger.cpp
+++ bbstlogger.cpp
@@ -5,6 +5,9 @@
  * Balloon Borne Solar Telescope (BBST)
  */
 #include <iostream>
+#include <algorithm>
+#include <iomanip>
+#include <map>
 #include <fstream>
 #include <thread>
 #include <vector>
@@ -46,6 +49,26 @@ struct
         uint8_t rollBuff[sizeof(float)];
     };
 } IMUComm;
+
+// Print matrix nicely. Used in photodiode thread.
+void printMat()
+{
+    std::cout << std::showbase
+              << std::internal
+              << std::setfill('0');
+
+    std::cout << "-----------------------------------------------------------" << std::endl;
+
+    for (const auto &row : photodiodes)
+    {
+        for (const auto &col : row)
+        {
+            std::cout << std::hex << std::setw(6) << col << " ";
+        }
+        std::cout << std::endl
+                  << std::endl;
+    }
+}
 
 // Write a vector of data to logfile as a comma separated line
 // with timestamp. vector holds strings, this might suck so review this.
@@ -96,14 +119,6 @@ void IMUThread()
         return;
     }
 
-    // Initialize WiringPi
-    if (wiringPiSetup() == -1)
-    {
-        serialClose(controllerFd);
-        std::cerr << "Unable to start WiringPi" << std::endl;
-        return;
-    }
-
     // Main IMU loop
     int i;
     float azimuth = 0.0f, elevation = 0.0f;
@@ -144,7 +159,7 @@ void IMUThread()
         data[2] = "Roll: " + std::to_string(IMUComm.roll);
 
         // Log data to logfile
-        //logfileWrite(data);
+        // logfileWrite(data);
 
         // TODO: Experimentally determine lower limit on delay between loops
         std::this_thread::sleep_for(std::chrono::milliseconds(50));
@@ -157,8 +172,78 @@ void IMUThread()
 
 void photodiodeThread()
 {
-    std::vector<std::string> data{"d7", "d4", "d1"};
-    logfileWrite(data);
+    const int PHOTODIODE_ARRAY_X = 5;
+    const int PHOTODIODE_ARRAY_Y = 5;
+
+    // GPIO pins we are using to control multiplexer channels
+    int pins[4] = {1, 2, 3, 4};
+
+    // Map the multiplexer channel bits to the corresponding
+    // index on the photodiodes array below.
+    std::map<uint8_t, std::vector<int>> photodiodeIdx{
+        {0x00, {0, 2}}, // 0b0000 corresponds to photodiode D1
+        {0x01, {1, 3}}, // 0b0001 corresponds to photodiode D2
+        {0x02, {2, 4}}, // 0b0010 corresponds to photodiode D3
+        {0x03, {3, 3}}, // 0b0011 corresponds to photodiode D4
+        {0x04, {4, 2}}, // 0b0100 corresponds to photodiode D5
+        {0x05, {3, 1}}, // 0b0101 corresponds to photodiode D6
+        {0x0A, {2, 0}}, // 0b1010 corresponds to photodiode D7
+        {0x0B, {1, 1}}, // 0b1011 corresponds to photodiode D8
+    };
+
+    // Store photodiode readings in this array such that their
+    // index mimics their physical position on the photodiode board.
+    // Below is the exact indexing:
+    //      D1 => photodiodes[0][2]
+    //      D2 => photodiodes[1][3]
+    //      D3 => photodiodes[2][4]
+    //      D4 => photodiodes[3][3]
+    //      D5 => photodiodes[4][2]
+    //      D6 => photodiodes[3][1]
+    //      D7 => photodiodes[2][0]
+    //      D8 => photodiodes[1][1]
+    // All other values in between are zeros and disgarded in processing.
+    uint16_t photodiodes[PHOTODIODE_ARRAY_Y][PHOTODIODE_ARRAY_X] = {
+        {0x0000, 0x0000, 0x0000, 0x0000, 0x0000},
+        {0x0000, 0x0000, 0x0000, 0x0000, 0x0000},
+        {0x0000, 0x0000, 0x0000, 0x0000, 0x0000},
+        {0x0000, 0x0000, 0x0000, 0x0000, 0x0000},
+        {0x0000, 0x0000, 0x0000, 0x0000, 0x0000}};
+
+    // Setup GPIO pins used for selecting mux channels
+    for (const auto &pin : pins)
+    {
+        pinMode(pin, OUTPUT);
+    }
+
+    // Setup ADC
+    Adafruit_ADS1015 ads;
+    ads.setGain(GAIN_ONE);
+    ads.begin();
+
+    while (1)
+    {
+        for (const auto &[key, val] : photodiodeIdx)
+        {
+            // Write key to GPIO pins to select particular photodiode
+            for (int i = 0; i < 4; i++)
+            {
+                digitalWrite(pins[i], (key >> i) & 0x01);
+            }
+
+            // TODO: Is this needed?
+            // Sleep for 1ms for good measuer
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+            // Read value from adc and store in photodiodes matrix
+            // TODO: Double check all values are coming from A0,
+            // or else we need to keep track of that too
+            photodiodes[val[0]][val[1]] = ads.readADC_SingleEnded(0);
+        }
+
+        printMat();
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    }
 }
 
 void imgProcThread()
@@ -169,15 +254,22 @@ void imgProcThread()
 
 int main()
 {
+    // Initialize WiringPi
+    if (wiringPiSetup() == -1)
+    {
+        std::cerr << "Unable to start WiringPi" << std::endl;
+        return;
+    }
+
     // Launch threads
     std::thread t_imu(IMUThread);
-    //std::thread t_photodiode(photodiodeThread);
-    //std::thread t_imgProc(imgProcThread);
+    // std::thread t_photodiode(photodiodeThread);
+    // std::thread t_imgProc(imgProcThread);
 
     // Join threads
     t_imu.join();
-    //t_photodiode.join();
-    //t_imgProc.join();
+    // t_photodiode.join();
+    // t_imgProc.join();
 
     return 1;
 }
